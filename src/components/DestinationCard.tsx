886757beff16720c14a216ccd78f6b7f919526bb
Philip Alegret <philip.alegret@gmail.com>
2024-06-28 15:54:15 -0400

    ESLint and Prettier changes.

--- src/components/DestinationCard.tsx
+++ src/components/DestinationCard.tsx
@@ -4,15 +4,28 @@ interface IDestinationCardProps {
   destination: IDestination;
 }
 
-export default function DestinationCard({ destination }: IDestinationCardProps) {
+export default function DestinationCard({
+  destination,
+}: IDestinationCardProps) {
   return (
-    <div className="flex items-center rounded-lg bg-white shadow-lg overflow-hidden">
-      <img className="h-32 w-32 flex-shrink-0" src={destination.imageUrl} alt={destination.imageAlt} />
+    <div className="flex items-center overflow-hidden rounded-lg bg-white shadow-lg">
+      <img
+        className="h-32 w-32 flex-shrink-0"
+        src={destination.imageUrl}
+        alt={destination.imageAlt}
+      />
       <div className="px-6 py-4">
-        <h3 className="text-lg font-semibold text-gray-800">{destination.city}</h3>
-        <p className="text-gray-600">${destination.averagePrice} / night average</p>
+        <h3 className="text-lg font-semibold text-gray-800">
+          {destination.city}
+        </h3>
+        <p className="text-gray-600">
+          ${destination.averagePrice} / night average
+        </p>
         <div className="mt-4">
-          <a href="#" className="text-indigo-600 hover:text-indigo-500 font-semibold text-sm">
+          <a
+            href="#"
+            className="text-sm font-semibold text-indigo-600 hover:text-indigo-500"
+          >
             Explore {destination.propertyCount} properties
           </a>
         </div>
